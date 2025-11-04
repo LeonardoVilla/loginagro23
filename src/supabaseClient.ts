5ae22c4562f0b0ce51a07fe0c2c445af2aac7b15
Leonardo da Silva Villa <leosivi@gmail.com>
2025-11-03 21:48:37 -0400

    Atualização com validação de login supbase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,22 @@
 //npm install @supabase/supabase-js
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from "@supabase/supabase-js";
+
+//verifica qual plataforma esta em uso
+import { Platform } from 'react-native';
 
 //Lendo as variavis do .env
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabasePublishableKey = supabaseAnonKey;
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    // storage: typeof window !== "undefined" ? window.localStorage : undefined ,
+    storage: Platform.OS === 'web' ? undefined : AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
